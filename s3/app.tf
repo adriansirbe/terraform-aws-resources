d3409d9c20614c66acb418cbbe6ae1860c5351fc
Adrian Sîrbe <adriansirbe@Adrians-MacBook-Pro.local>
2020-08-17 08:15:30 +0300

    Add userdata template

--- s3/app.tf
+++ s3/app.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "web-nodejs-app" {
-  bucket = "web-nodejs-app"
+resource "aws_s3_bucket" "basic-web-application-2020" {
+  bucket = "basic-web-application-2020"
 
   versioning {
         enabled = true
     }
 
   tags = {
-    Name        = "web-nodejs-app"
+    Name        = "basic-web-application-2020"
   }
 }
